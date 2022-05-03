ab027b340dd74a8e0a8ba5f640ecaf15a342d46c
leehaakjoo <l--h--j--@naver.com>
2022-05-03 21:54:51 +0900

    Add:: Carousel

--- goodjob-front/src/views/main/MainPresenter.js
+++ goodjob-front/src/views/main/MainPresenter.js
@@ -1,31 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as s from './MainStyled';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const MainPresenter = ({...props}) => {
+
+    function Arrow(props) {
+        const { className, style, onClick } = props;
+        return <div className={className} style={{ ...style, display: 'block', background: 'gray', outline: 'none', borderRadius: '10px'}} onClick={onClick} />;
+    }
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        centerMode: true,
+        nextArrow: <Arrow/>,
+        prevArrow: <Arrow/>,
+    };
     const {userData, companyList} = props;
-    return(
+    return (
         <s.Container>
             <s.MainBlock>
-                {
-                    companyList.length > 0 ? 
-                        companyList.map((company) => (
-                            <Link key={company.companyName}
-                                to={`/company`}
-                                state={{
-                                    workGroup: userData.job_group,
-                                    company: company.companyName,
-                                }}
-                            >
-                                {company.companyName}
-                            </Link>
-                        ))
+                <s.MainHeader>적합기업 추천 결과</s.MainHeader>
 
-                    : null
-                }
+                <s.MainCarousel>
+                    <Slider {...settings}>
+                        {companyList.length > 0
+                            ? companyList.map((company) => (
+                                  <s.CardContainer>
+                                      <s.Card>
+                                          <Link
+                                              key={company.name}
+                                              to={`/company`}
+                                              state={{
+                                                  workGroup: userData.job_group,
+                                                  company: company.name,
+                                              }}
+                                              style={{ textDecoration: 'none', textDecorationLine: 'none' }}
+                                          >
+                                              <s.CardContent>
+                                                  <s.CompanyName>{company.name}</s.CompanyName>
+                                              </s.CardContent>
+                                          </Link>
+                                      </s.Card>
+                                  </s.CardContainer>
+                              ))
+                            : null}
+                    </Slider>
+                </s.MainCarousel>
+                <s.MainOther></s.MainOther>
             </s.MainBlock>
         </s.Container>
-
     );
 }
 
--- goodjob-front/src/views/main/MainStyled.js
+++ goodjob-front/src/views/main/MainStyled.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export const Container = styled.div`
-
+    
     margin-top: 3rem;
     box-shadow: 0px 0px 10px #f3f6fb;
     margin-bottom: 300px;
@@ -10,13 +13,91 @@ export const Container = styled.div`
     display: inline-block;
     background-color: white;
     width:85%;
+    max-width: 1000px;
     @media screen and (min-width: 1180px) {
         min-width: 80%;
     }
+
+
 `;
 
 export const MainBlock = styled.div`
     text-align: left;
     width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const MainHeader = styled.div`
+    padding: 30px;
+    font-size: 35px;
+    font-weight: bold;
+    color: #3cb371;
+    flex: 1;
 
 `;
+
+export const MainCarousel = styled.div`
+    flex: 6;
+    width: 100%;
+    @media screen and (min-width: 767px) {
+        max-width: 500px;
+    }
+
+    @media screen and (min-width: 1080px) {
+        min-width: 900px;
+    }
+
+    align-self: center;
+    justify-self: center;
+`;
+
+export const MainOther = styled.div`
+    flex: 2;
+
+`;
+
+export const StyledSlider = styled(Slider)`
+    .slick-slide div{
+        outline: none;
+    }
+`;
+
+export const CardContainer = styled.div`
+    margin: 5px;
+`;
+
+export const Card = styled.div`
+    height: 300px;
+
+    @media screen and (min-width: 767px) {
+        padding: 1rem;
+    }
+
+    @media screen and (min-width: 1080px) {
+        padding: 2rem;
+    }
+`;
+
+export const CardContent = styled.div`
+    padding: 1rem;
+    background-color: #3cb371;
+    border-radius: 20px;
+    height: 100%;
+    color: white;
+    &:hover{
+        background-color: green;
+        opacity: 0.4;
+    }
+`;
+
+export const CompanyName = styled.div`
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+    @media screen and (min-width: 767px) {
+        font-size: 15px;
+    }
+`;
+
